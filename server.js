ad83152eb0cb1750f3794a3ba679c978f0b41a9d
Joseph Barbone <JoeBarbone@aol.com>
2022-06-22 15:38:57 -0400

    added single id search and heroku hosting

--- server.js
+++ server.js
@@ -49,6 +49,13 @@ function filterByQuery(query, animalsArray) {
     return filteredResults;
 }
 
+
+function findById(id, animalsArray) {
+    const result = animalsArray.filter(animal => animal.id === id)[0];
+    return result;
+}
+
+
 app.get("/api/animals", (req, res) => {
     
     let results = animals;
@@ -60,6 +67,19 @@ app.get("/api/animals", (req, res) => {
 
 });
 
+app.get("/api/animals/:id", (req, res) => {
+
+    const result = findById(req.params.id, animals);
+    if (result) {
+        res.json(result);
+    } else {
+        res.send(404);
+    }
+
+});
+
 app.listen(PORT, () => {
+
     console.log(`API Server now on port ${PORT}`);
+
 });
